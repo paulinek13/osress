9e24a19e044af58c6e48d95c51356a02fad3495c
Paulina Kalicka <paulinek13@outlook.com>
2024-10-30 18:06:48 +0100

    feat(ui): add top navigation bar to site layout

---
+++ src/src/lib/ui/layout/TopNav.svelte
@@ -0,0 +1,18 @@
+<script>
+	import NavLink from '../common/NavLink.svelte';
+</script>
+
+<nav class="bg-neutral-100 text-black">
+	<a class="flex" href="/">
+		<div class="flex-1 bg-[#7D0000]"></div>
+		<img class="" src="./top.svg" alt="OSRESS" />
+		<div class="flex-1 bg-neutral-100"></div>
+	</a>
+
+	<div class="bg-neutral-100 text-black font-bold flex flex-wrap justify-center">
+		<NavLink href="/news">News</NavLink>
+		<NavLink href="/consortium">Consortium</NavLink>
+		<NavLink href="/editions">Editions</NavLink>
+		<NavLink href="/media">Pictures & Videos</NavLink>
+	</div>
+</nav>
