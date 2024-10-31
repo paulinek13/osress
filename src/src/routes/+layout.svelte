71f00b2770139fe99c4d3690d5561d11f253ebdf
Paulina Kalicka <paulinek13@outlook.com>
2024-10-31 22:15:08 +0100

    feat(ui): update main `+layout.svelte` and add `top.jpg` image

--- src/src/routes/+layout.svelte
+++ src/src/routes/+layout.svelte
@@ -1,10 +1,26 @@
 <script>
-	import "../app.css";
+	import '../app.css';
+
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+
+	import Footer from '$lib/ui/layout/Footer.svelte';
+	import TopNav from '$lib/ui/layout/TopNav.svelte';
+
 	let { children } = $props();
 </script>
 
-<ParaglideJS {i18n}>
-	{@render children()}
-</ParaglideJS>
+<div
+	id="OSRESS"
+	class="fixed top-0 left-0 bottom-0 w-full overflow-auto flex flex-col bg-neutral-50 text-black text-base"
+>
+	<TopNav />
+
+	<main class="flex-1">
+		<ParaglideJS {i18n}>
+			{@render children()}
+		</ParaglideJS>
+	</main>
+
+	<Footer />
+</div>
