9fc1f97c9adddb2076ea95549d8c9d245f1431d8
Paulina Kalicka <paulinek13@outlook.com>
2024-11-09 00:14:03 +0100

    feat(ui): implement dynamic routing for picture galleries
    Added dynamic routing to load picture galleries based on the year parameter in the URL.
    Currently, images for the 2013 and 2019 editions are available.

---
+++ src/src/routes/pictures/[year]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import GalleryBlock from '$lib/ui/blocks/GalleryBlock.svelte';
+
+	export let data;
+
+	let imageCounts = {
+		'2013': 56,
+		'2019': 155
+	};
+	let images = [];
+	let count = imageCounts[data.year] || 0;
+	for (let i = 0; i < count; i++) {
+		images.push(
+			`https://osress-${data.year}.pages.dev/osress_${data.year}_${i.toString().padStart(3, '0')}.jpg`
+		);
+	}
+</script>
+
+<GalleryBlock {images} />
