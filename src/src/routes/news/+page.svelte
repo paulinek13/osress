cfaa302077e8f02498223cc6f04361795bcc9295
Paulina Kalicka <paulinek13@outlook.com>
2024-11-14 00:14:39 +0100

    feat(content): add 'News' page

---
+++ src/src/routes/news/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center gap-4 py-16 px-8 md:py-32 md:px-16">
+	<h1 class="text-2xl font-bold">News</h1>
+	<p>Stay tuned! We’ll be announcing the host city for the next edition very soon—don’t miss it!</p>
+</div>
