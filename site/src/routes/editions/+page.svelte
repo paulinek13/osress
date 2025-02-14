b0480ff3315708a5f6567c8ecfef28cb77c3cb11
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-02-14 07:20:28 +0100

    style: adjust layout for editions page to improve responsiveness

--- site/src/routes/editions/+page.svelte
+++ site/src/routes/editions/+page.svelte
@@ -2,7 +2,7 @@
 	export let data;
 </script>
 
-<div class="p-4 flex flex-wrap items-center justify-center gap-2 mt-4">
+<div class="p-4 flex flex-wrap items-center justify-center gap-2 mt-4 max-w-xl m-auto">
 	{#each data.editions as edition}
 		<a
 			class="border border-black text-black hover:bg-black hover:text-neutral-200 px-8 py-2 transition-all duration-200"
